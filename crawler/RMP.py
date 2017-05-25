efea984f72f565fce4d89870a96cd62fdd0acde8
Chao Ma <ynyounuo@me.com>
2017-05-25 03:35:33 -0700

    Reformat the crawlers

--- crawler/RMP.py
+++ crawler/RMP.py
@@ -8,20 +8,21 @@ PROF_RATING_URL = 'http://www.ratemyprofessors.com/paginate/professors/ratings'
 rows = 10000
 
 uni_params = {
-	'solrformat':'true',
-	'rows':rows,
-	'callback':'',
-	'q':'*:* AND schoolid_s:1530',
-	'defType':'edismax',
-	'qf':'teacherfirstname_t^2000 teacherlastname_t^2000 teacherfullname_t^2000 autosuggest',
-	'bf':'pow(total_number_of_ratings_i,2.1)',
-	'sort':'total_number_of_ratings_i desc',
-	'siteName':'rmp',
-	'rows':rows,
-	'start':0,
-	'fl':'pk_id teacherfirstname_t teacherlastname_t total_number_of_ratings_i averageratingscore_rf schoolid_s',
-	'fq':'',
-	'prefix':'schoolname_t:"University of Washington"',
+    'solrformat': 'true',
+    'rows': rows,
+    'callback': '',
+    'q': '*:* AND schoolid_s:1530',
+    'defType': 'edismax',
+    'qf': 'teacherfirstname_t^2000 teacherlastname_t^2000 '
+          'teacherfullname_t^2000 autosuggest',
+    'bf': 'pow(total_number_of_ratings_i,2.1)',
+    'sort': 'total_number_of_ratings_i desc',
+    'siteName': 'rmp',
+    'start': 0,
+    'fl': 'pk_id teacherfirstname_t teacherlastname_t '
+          'total_number_of_ratings_i averageratingscore_rf schoolid_s',
+    'fq': '',
+    'prefix': 'schoolname_t:"University of Washington"',
 }
 
 r = requests.get(UNI_URL, params=uni_params)
@@ -31,35 +32,34 @@ tids = []
 # Get first, last, score and tid.
 docs = r.json()['response']['docs']
 for professor in docs:
-	if not 'averageratingscore_rf' in professor or professor['averageratingscore_rf'] == 0:
-		continue
+    if not ('averageratingscore_rf' in professor and not (
+                professor['averageratingscore_rf'] == 0)):
+        continue
 
-	first = professor['teacherfirstname_t']
-	last = professor['teacherlastname_t']
-	score = professor['averageratingscore_rf']
-	pk_id = professor['pk_id']
+    first = professor['teacherfirstname_t']
+    last = professor['teacherlastname_t']
+    score = professor['averageratingscore_rf']
+    pk_id = professor['pk_id']
 
-	tids.append(pk_id)
-	print(first, last, score)
+    tids.append(pk_id)
+    print(first, last, score)
 
 # Get reviews for each of the professors
 for tid in tids:
-	page_num = 1
-	while True:
-		r = requests.get(PROF_RATING_URL, params={'tid':tid, 'page':page_num})
-		json = r.json()
-		ratings = json['ratings']
-		remaining = json['remaining']
+    page_num = 1
+    while True:
+        r = requests.get(PROF_RATING_URL, params={'tid': tid, 'page': page_num})
+        json = r.json()
+        ratings = json['ratings']
+        remaining = json['remaining']
 
-		for rating in ratings:
-			course = rating['rClass']
-			overall = rating['rOverall']
-			difficulty = rating['rEasy']
+        for rating in ratings:
+            course = rating['rClass']
+            overall = rating['rOverall']
+            difficulty = rating['rEasy']
 
-			print(course, overall, difficulty)
+            print(course, overall, difficulty)
 
-		if remaining == 0:
-			break
-		page_num += 1
-
-
+        if remaining == 0:
+            break
+        page_num += 1
--- crawler/schedule.py
+++ crawler/schedule.py
@@ -2,34 +2,39 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+
 def tab_serparate(s):
-	return re.sub(r'\W{2,}|-', '\t', s)
+    return re.sub(r'\W{2,}|-', '\t', s)
+
 
 def crawl_subject(url):
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text, 'html.parser')
-	course_name_elems = soup.select('table[bgcolor=#ffcccc]')
-	for elem in course_name_elems:
-		# Subject name
-		subject_name_line = elem.select('a[name]')[0].text
-		print(tab_serparate(subject_name_line))
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    course_name_elems = soup.select('table[bgcolor=#ffcccc]')
+    for elem in course_name_elems:
+        # Subject name
+        subject_name_line = elem.select('a[name]')[0].text
+        print(tab_serparate(subject_name_line))
 
-		for section_elem in elem.next_siblings:
-			if section_elem.name == 'br' or section_elem.name == 'p':
-				break
+        for section_elem in elem.next_siblings:
+            if section_elem.name == 'br' or section_elem.name == 'p':
+                break
 
-			if section_elem.name == 'table':
-				# Section table
-				section_table_line = section_elem.text
-				print(tab_serparate(section_table_line))
+            if section_elem.name == 'table':
+                # Section table
+                section_table_line = section_elem.text
+                print(tab_serparate(section_table_line))
 
 
 URL = 'https://www.washington.edu/students/timeschd/AUT2017/'
+
+
 def crawl_all_subjects(url):
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text, 'html.parser')
-	a_tags = soup.find_all(href=re.compile("^[a-z0-9]+\.html$"))
-	for a in a_tags:
-		crawl_subject(URL + a['href'])
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    a_tags = soup.find_all(href=re.compile("^[a-z0-9]+\.html$"))
+    for a in a_tags:
+        crawl_subject(URL + a['href'])
+
 
 crawl_all_subjects(URL)

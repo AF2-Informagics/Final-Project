d99b40d41b5583f334bee02f0a8d94148b15f1a0
Chao Ma <ynyounuo@me.com>
2017-05-27 19:08:59 -0700

    Merge scripts into one

--- scripts/dataframe.R
+++ scripts/dataframe.R
@@ -1,11 +1,24 @@
 # setwd("~/Dropbox/UW/Courses/INFO 201/Final-Project")
 library(dplyr)
+library(stringr)
+
 df <- read.csv(file = "data/schedule.csv", stringsAsFactors = FALSE)
 rooms <- df %>% select(Building, Room) %>% group_by(Building) %>% filter(Building != "")
 course.df <- df %>% select(Course) %>% filter(Course != "")
 course.split <- data.frame(do.call('rbind', strsplit(as.character(course.df$Course), ' (?=[^ ]+$)', perl=TRUE)))
 colnames(course.split) <- c("Course", "Number")
 
+course <- unique(df$Course[df$Course != ""])
+course.letter <- unique(gsub("[[:digit:]]","",course))
+course.letter <- trimws(course.letter)
+
+course.list <- list()
+MakeList <- function(abbr) {
+  vec <- grep(abbr, course, value = TRUE)
+  mylist <- list()
+  mylist[[abbr]] <- vec
+  return(c(course.list, mylist))
+}
 RemoveEmpty <- function(data) {
   data[!apply(data == "", 1, all),]
 }
@@ -14,18 +27,13 @@ RemoveEmptyNA <- function(data) {
   data <- data[!apply(is.na(data) | data == "", 1, all),]
 }
 
-MakeList <- function(building, df) {
-  data <- df %>% filter(Building == building) %>% ungroup() %>% select(Room)
+GetRoom <- function(building) {
+  data <- rooms %>% filter(Building == building) %>% ungroup() %>% select(Room)
   list <- list(data)
 }
 
-MakeList2 <- function(course) {
-  data <- course.split %>% select(Number)
-  list <- list(data)
-}
-
-rooms.list <- sapply(buildings, MakeList, df = rooms)
-course.list <- sapply(course.split$Course, MakeList2)
+rooms.list <- sapply(buildings, GetRoom)
+course.list <- lapply(course.letter, MakeList)
 
 courses <- unique(df$Course[df$Course != ""])
 buildings <- unique(df$Building[df$Building != ""])

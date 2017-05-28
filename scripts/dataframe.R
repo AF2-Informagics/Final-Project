fe77857c25a070febb43cc6bf1b95c1913542332
Chao Ma <ynyounuo@me.com>
2017-05-27 17:28:39 -0700

    Add a function to combine group rows by common value as a list

--- scripts/dataframe.R
+++ scripts/dataframe.R
@@ -1,4 +1,23 @@
 # setwd("~/Dropbox/UW/Courses/INFO 201/Final-Project")
+library(dplyr)
+
+RemoveEmpty <- function(data) {
+  data[!apply(data == "", 1, all),]
+}
+
+RemoveEmptyNA <- function(data) {
+  data <- data[!apply(is.na(data) | data == "", 1, all),]
+}
+
+rooms <- df %>% select(Building, Room) %>% group_by(Building) %>% RemoveEmpty() 
+
+MakeList <- function(building, df) {
+  data <- df %>% filter(Building == building) %>% ungroup() %>% select(Room)
+  list <- list(data)
+}
+
+rooms.list <- sapply(buildings, MakeList, df = rooms)
+
 df <- read.csv(file = "data/schedule.csv", stringsAsFactors = FALSE)
 courses <- unique(df$Course[df$Course != ""])
 buildings <- unique(df$Building[df$Building != ""])

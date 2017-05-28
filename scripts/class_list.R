d023d890e9e72bc087c9f6eb46892b63e38e56af
Virginiacxy <906033130@qq.com>
2017-05-27 18:05:17 -0700

    finished make list

--- scripts/class_list.R
+++ scripts/class_list.R
@@ -4,13 +4,17 @@ library(stringr)
 df <- read.csv("data/schedule.csv", stringsAsFactors = FALSE)
 course <- unique(df$Course[df$Course != ""])
 course.letter <- unique(gsub("[[:digit:]]","",course))
+course.letter <- trimws(course.letter)
 
 course.list <- list()
 MakeList <- function(abbr) {
   vec <- grep(abbr, course, value = TRUE)
-  return(list <- (abbr = vec))
+  mylist <- list()
+  mylist[[abbr]] <- vec
+  return(c(course.list, mylist))
 }
 
-course.list[[length(course.list)+1]] <- lapply(course.letter, MakeList)
+course.list <- lapply(course.letter, MakeList)
 
 # vec <- grep("MOLMED", course, value = TRUE)
+

a272e4a9d7566d142e1a8c75a365d932f3250278
Chao Ma <ynyounuo@me.com>
2017-05-27 18:16:47 -0700

    Add course list

--- scripts/dataframe.R
+++ scripts/dataframe.R
@@ -1,5 +1,10 @@
 # setwd("~/Dropbox/UW/Courses/INFO 201/Final-Project")
 library(dplyr)
+df <- read.csv(file = "data/schedule.csv", stringsAsFactors = FALSE)
+rooms <- df %>% select(Building, Room) %>% group_by(Building) %>% filter(Building != "")
+course.df <- df %>% select(Course) %>% filter(Course != "")
+course.split <- data.frame(do.call('rbind', strsplit(as.character(course.df$Course), ' (?=[^ ]+$)', perl=TRUE)))
+colnames(course.split) <- c("Course", "Number")
 
 RemoveEmpty <- function(data) {
   data[!apply(data == "", 1, all),]
@@ -9,15 +14,18 @@ RemoveEmptyNA <- function(data) {
   data <- data[!apply(is.na(data) | data == "", 1, all),]
 }
 
-rooms <- df %>% select(Building, Room) %>% group_by(Building) %>% RemoveEmpty() 
-
 MakeList <- function(building, df) {
   data <- df %>% filter(Building == building) %>% ungroup() %>% select(Room)
   list <- list(data)
 }
 
-rooms.list <- sapply(buildings, MakeList, df = rooms)
+MakeList2 <- function(course) {
+  data <- course.split %>% select(Number)
+  list <- list(data)
+}
 
-df <- read.csv(file = "data/schedule.csv", stringsAsFactors = FALSE)
+rooms.list <- sapply(buildings, MakeList, df = rooms)
+course.list <- sapply(course.split$Course, MakeList2)
+
 courses <- unique(df$Course[df$Course != ""])
 buildings <- unique(df$Building[df$Building != ""])

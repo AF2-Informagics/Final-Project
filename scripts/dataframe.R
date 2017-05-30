fe1fb9c5768928bbb27436b031f5a8294268d138
Chao Ma <ynyounuo@me.com>
2017-05-30 00:42:20 -0700

    Reformat the code

--- scripts/dataframe.R
+++ scripts/dataframe.R
@@ -3,21 +3,25 @@ library(dplyr)
 library(stringr)
 library(jsonlite)
 
-df <- read.csv(file = "data/schedule_new.csv", stringsAsFactors = FALSE)
-# mutate(df, test = ifelse(grepl("P",df$EndTime), StartTime + 1200, StartTime))
-# mutate(df, test2 = ifelse(df$StartTime < 700, df$StartTime + 1200, df$StartTime))
+df <- read.csv(file = "data/schedule.csv", stringsAsFactors = FALSE)
+
 df$StartTime_new <- ifelse((df$StartTime < 700 | grepl("P",df$EndTime)), df$StartTime + 1200, df$StartTime)
 df$EndTime_new <- ifelse((df$StartTime < 700 | (df$StartTime <= 1200 & as.integer(gsub("([0-9]+).*$", "\\1", df$EndTime)) < df$StartTime) | grepl("P", df$EndTime) | df$StartTime > 1200), as.integer(gsub("([0-9]+).*$", "\\1", df$EndTime)) + 1200, suppressWarnings(as.integer(df$EndTime)))
+df$StartTime_new <- substr(as.POSIXct(sprintf("%04.0f", df$StartTime_new), format='%H%M'),12,16)
+df$EndTime_new <- substr(as.POSIXct(sprintf("%04.0f", df$EndTime_new), format='%H%M'),12,16)
 
 building_info <- fromJSON("data/parse.json")
+building_info <- as.data.frame(building_info, stringsAsFactors = FALSE)
 
 rooms <- df %>% select(Building, Room) %>% group_by(Building) %>% filter(Building != "")
 courses.df <- df %>% select(Course) %>% filter(Course != "")
+
 # split the courses name by the final space, e.g. 'SOC W 308' to 'SOC W' and '308'
 courses.split <- data.frame(do.call('rbind', strsplit(as.character(courses.df$Course), ' (?=[^ ]+$)', perl=TRUE)), stringsAsFactors = FALSE)
+within(df, courses.df$Course<-data.frame(do.call('rbind', strsplit(as.character(courses.df$Course), ' (?=[^ ]+$)', perl=TRUE)), stringsAsFactors =  FALSE))
+
 colnames(courses.split) <- c("Course", "Number")
 
-# 
 RemoveEmpty <- function(data) {
   data[!apply(data == "", 1, all),]
 }
@@ -42,8 +46,5 @@ departments <- unique(courses.split$Course[courses.split$Course != ""])
 
 rooms.list <- sapply(buildings, GetRoom)
 courses.list <- sapply(departments, GetCourse)
-
-df$StartTime_new <- substr(as.POSIXct(sprintf("%04.0f", df$StartTime_new), format='%H%M'),12,16)
-df$EndTime_new <- substr(as.POSIXct(sprintf("%04.0f", df$EndTime_new), format='%H%M'),12,16)
 
 # write.csv(df, file = "data/schedule_new.csv", row.names = FALSE)

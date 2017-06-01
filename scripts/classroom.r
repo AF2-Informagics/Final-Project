1ab0c895a1960ae7185e6fb28635ee75d456b7d5
Chao Ma <ynyounuo@me.com>
2017-06-01 03:31:39 -0700

    Add classroom.R to calculate the empty classroom

---
+++ scripts/classroom.r
@@ -0,0 +1,13 @@
+source(file = "scripts/dataframe.R")
+temp.time <- Sys.time() + 3600 * 11
+
+new.df <- df %>% mutate(availbility = StartTime_new > temp.time | temp.time > EndTime_new) %>% select(Building, Room, availbility) %>% filter(Building != "", Room != "") %>% distinct()
+
+true.df <- new.df %>% filter(availbility == TRUE) %>% select(Building, Room)
+
+false.df <- new.df %>% filter(availbility == FALSE) %>% select(Building, Room)
+
+temp.df <- inner_join(true.df, false.df)
+available.df <- setdiff(true.df, temp.df)
+
+available.df.num <- available.df %>% group_by(Building) %>% summarise(num = n())
